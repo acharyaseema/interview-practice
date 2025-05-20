37220aed72975cb403b72cc35d95161fe0354b12
Seema <seema.ach@gmail.com>
2025-05-19 22:35:24 -0700

    Test

---
+++ src/main/java/Solution.java
@@ -0,0 +1,6 @@
+// This is a simple Java program that prints "Hello" to the console.
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
